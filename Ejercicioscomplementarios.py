00af10fd8fd28e897870e2c188ad068a20d74f8e
Orihuelaa <133928810+Orihuelaa@users.noreply.github.com>
2023-08-17 08:12:41 -0300

    Add files via upload

---
+++ Ejercicioscomplementarios.py
@@ -0,0 +1,75 @@
+# Ejercicio 1
+numero1 = 5
+print(numero1)
+
+# Ejercicio 2
+numero2 = 3.85
+print(numero2)
+
+# Ejercicio 3
+suma = numero1 + numero2
+print(suma)
+
+# Ejercicio 4
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+print(resta)
+print(multiplicacion)
+print(division)
+
+# Ejercicio 5
+nombre = "Franco"
+print(nombre)
+
+# Ejercicio 6
+precio = 88.99
+print(precio)
+
+# Ejercicio 7
+descuento = 0.25
+print(descuento)
+
+# Ejercicio 8
+precio_final = precio-(precio * descuento)
+#Otra Forma (precio_final = precio * (1 - descuento))
+print(precio_final)
+
+# Ejercicio 9
+cadena = "Amo programar"
+print(cadena)
+
+# Ejercicio 10
+longitud = len(cadena)
+print(longitud)
+
+# Ejercicio 11
+precio = 45.67
+precio_entero = int(precio)
+print(precio_entero)
+
+# Ejercicio 12
+nombre = "Franco"
+apellido = "Orihuela"
+nombre_completo = nombre + " " + apellido
+print(nombre_completo)
+
+# Ejercicio 13
+edad = 20
+edad += 5
+edad -= 10
+print(edad)
+
+# Ejercicio 14
+altura = 1.67
+altura *= 4
+altura /= 3
+print(altura)
+
+# Ejercicio 15
+nombre_mayusculas = nombre_completo.upper()
+print(nombre_mayusculas)
+
+# Ejercicio 16
+primera_mayuscula_apellido = nombre_completo.title()
+print(primera_mayuscula_apellido)
